0d6b1ab52c3e2144694a6340383d35fbed9d6263
Nathaniel Mon Père <47852617+natevmp@users.noreply.github.com>
2026-04-07 16:21:37 +0100

    Testing + particle parameter format

--- src/ABCRejection.jl
+++ src/ABCRejection.jl
@@ -15,43 +15,52 @@ end
         params_tid::Vector{<:NamedTuple},
         ctrlParams::Union{Dict,NamedTuple}=NamedTuple();
         verbose::Bool=false,
-        randParams::Bool=true
     )
 
 Create multiple particles for the parameters in `params_tid`
 """
-function runABCParticles(runModelSim::Function, params_tid::Vector{<:NamedTuple}, ctrlParams::Union{Dict,NamedTuple}=NamedTuple(); verbose::Bool=false, randParams::Bool=false)
+function runABCParticles(runModelSim::Function, params_tid::Vector{<:NamedTuple}, ctrlParams::Union{Dict,NamedTuple}=(;); verbose::Bool=false)
     particle_tid = Vector{Particle}(undef, length(params_tid))
     for tid in 1:length(params_tid)
-        particle_tid[tid] = runParticle(runModelSim, params_tid[tid], ctrlParams; verbose, randParams)
+        particle_tid[tid] = runParticle(runModelSim, params_tid[tid], ctrlParams; verbose)
     end
     return particle_tid
 end
 
-function runABCParticles(runModelSim::Function, priorDists_pid::NamedTuple, nParticles::Integer, ctrlParams::Dict; verbose::Bool=false, randParams::Bool=true)
-    particle_tid = Vector{Particle}(undef, nParticles)
-    for tid in 1:nParticles
-        particle_tid[tid] = runParticle(runModelSim, priorDists_pid, ctrlParams; verbose, randParams)
-    end
-    return particle_tid
-end
-
-# function drawParams()
-    
-# end
-
-function runParticle(runModelSim::Function, pDist_pid::NamedTuple, ctrlParams::Union{Dict, NamedTuple}; verbose::Bool=false, randParams::Bool=true)
-    pVal_pid = 
-        if randParams
-            [rand(pDist) for pDist in pDist_pid]
-        else
-            pDist_pid
+function runABCParticles(runModelSim::Function, params_tid_Pid::NamedTuple{Names, <:Tuple{Vararg{AbstractVector}}} where Names, ctrlParams::Union{Dict,NamedTuple}=(;); verbose::Bool=false)
+    nParticles = length(first(params_tid_Pid))
+    for (pName, params_tid) in pairs(params_tid_Pid)
+        if length(params_tid) != nParticles
+            throw(ArgumentError("All parameter vectors in `params_tid_Pid` must have the same length. Parameter `$pName` has length $(length(params_tid)) while the first parameter has length $nParticles."))
         end
-    paramSet = (; zip(keys(pDist_pid), pVal_pid)...)
+    end
+
+    particle_tid = Vector{Particle}(undef, nParticles)
+    pNames = keys(params_tid_Pid) |> Tuple
+    for tid in eachindex(particle_tid)
+        pVal_pid = NamedTuple{pNames}(Tuple(pVal_tid[tid] for pVal_tid in params_tid_Pid))
+        particle_tid[tid] = runParticle(runModelSim, pVal_pid, ctrlParams; verbose)
+    end
+    return particle_tid
+end
+
+function drawParams(priorDist_pid::Union{NamedTuple,Dict}, nParticles::Integer)
+    _pNames = keys(priorDist_pid) |> Tuple
+    params_tid_Pid = NamedTuple{_pNames}(Tuple(rand(dist, nParticles) for dist in values(priorDist_pid)))
+    return params_tid_Pid
+end
+
+function runABCParticles(runModelSim::Function, priorDist_pid::Union{NamedTuple,Dict}, nParticles::Integer, ctrlParams::Union{Dict,NamedTuple}=(;); verbose::Bool=false)
+    params_tid_Pid = drawParams(priorDist_pid, nParticles)
+    runABCParticles(runModelSim, params_tid_Pid, ctrlParams; verbose)
+end
+
+function runParticle(runModelSim::Function, pVal_pid::NamedTuple, ctrlParams::Union{Dict, NamedTuple}; verbose::Bool=false)
+    paramSet = pVal_pid # Use the passed NamedTuple directly
     simResults = runModelSim(paramSet, ctrlParams)
     if verbose
         println("parameter values of particle:")
-        for (i,pid) in enumerate(keys(pDist_pid))
+        for (i,pid) in enumerate(keys(pVal_pid))
             println(string(pid)*": ", string(pVal_pid[i]))
         end
     end
@@ -121,74 +130,74 @@ function rankABCParticlesJointQuantiles(
     return tid_orderStatJoint
 end
 
-function testABCParticlesQuantiles(
-        distDataVSim::Function,
-        particle_tid::Vector,
-        dataMetrics,
-        q::Real,
-        constraintsF::Union{Nothing,Function}=nothing;
-    )
-    nMetrics=length(dataMetrics)
-    distance_tid_mid = getParticleDistancesPerMetric(distDataVSim, particle_tid, dataMetrics)
-    q_mid = [quantile((@view distance_tid_mid[:,mid]), q) for mid in 1:nMetrics]
-    accepted_tid = [all(distance_tid_mid[tid,:].<q_mid) for tid in eachindex(particle_tid)]
-    if isnothing(constraintsF)
-        return accepted_tid
-    end
-    acceptedConstraints_tid = falses(length(particle_tid))
-    for (tid, particle) in enumerate(particle_tid)
-        acceptedConstraints_tid[tid] = all(constraintsF(particle.simResults))
-    end
-    return accepted_tid .&& acceptedConstraints_tid
-end
+# function testABCParticlesQuantiles(
+#         distDataVSim::Function,
+#         particle_tid::Vector,
+#         dataMetrics,
+#         q::Real,
+#         constraintsF::Union{Nothing,Function}=nothing;
+#     )
+#     nMetrics=length(dataMetrics)
+#     distance_tid_mid = getParticleDistancesPerMetric(distDataVSim, particle_tid, dataMetrics)
+#     q_mid = [quantile((@view distance_tid_mid[:,mid]), q) for mid in 1:nMetrics]
+#     accepted_tid = [all(distance_tid_mid[tid,:].<q_mid) for tid in eachindex(particle_tid)]
+#     if isnothing(constraintsF)
+#         return accepted_tid
+#     end
+#     acceptedConstraints_tid = falses(length(particle_tid))
+#     for (tid, particle) in enumerate(particle_tid)
+#         acceptedConstraints_tid[tid] = all(constraintsF(particle.simResults))
+#     end
+#     return accepted_tid .&& acceptedConstraints_tid
+# end
 
-function testABCParticles(compareDataVSim::Function, particle_tid::Vector, dataMetrics, errorThresholds, constraintsF::Union{Nothing,Function}=nothing)
-    accepted_tid = falses(length(particle_tid))
-    for (tid,particle) in enumerate(particle_tid)
-        accepted_tid[tid] = testParticle(compareDataVSim, particle.simResults, dataMetrics, errorThresholds, constraintsF)
-    end
-    return accepted_tid
-end
+# function testABCParticles(compareDataVSim::Function, particle_tid::Vector, dataMetrics, errorThresholds, constraintsF::Union{Nothing,Function}=nothing)
+#     accepted_tid = falses(length(particle_tid))
+#     for (tid,particle) in enumerate(particle_tid)
+#         accepted_tid[tid] = testParticle(compareDataVSim, particle.simResults, dataMetrics, errorThresholds, constraintsF)
+#     end
+#     return accepted_tid
+# end
 
-function testParticle(compareDataVSim::Function, simResults, dataMetrics, errorThresholds, constraintsF::Union{Nothing,Function}=nothing)
-    acceptedMetrics = compareDataVSim(dataMetrics, simResults, errorThresholds)
-    if isnothing(constraintsF)
-        return all(acceptedMetrics)
-    else
-        acceptedConstraints = constraintsF(simResults)
-        return all(acceptedMetrics) && all(acceptedConstraints)
-    end
-end
+# function testParticle(compareDataVSim::Function, simResults, dataMetrics, errorThresholds, constraintsF::Union{Nothing,Function}=nothing)
+#     acceptedMetrics = compareDataVSim(dataMetrics, simResults, errorThresholds)
+#     if isnothing(constraintsF)
+#         return all(acceptedMetrics)
+#     else
+#         acceptedConstraints = constraintsF(simResults)
+#         return all(acceptedMetrics) && all(acceptedConstraints)
+#     end
+# end
 
-function measureABCParticles(compareDataVSimError::Function, particle_tid::Vector, dataMetrics)
-    dists1 = compareDataVSimError(dataMetrics, particle_tid[1].simResults)
-    dists_tid = Vector{typeof(dists1)}(undef, length(particle_tid))
-    dists_tid[1] = dists1
-    for tid in 2:length(particle_tid)
-        dists_tid[tid] = compareDataVSimError(dataMetrics, particle_tid[tid].simResults)
-    end
-    return dists_tid
-end
+# function measureABCParticles(compareDataVSimError::Function, particle_tid::Vector, dataMetrics)
+#     dists1 = compareDataVSimError(dataMetrics, particle_tid[1].simResults)
+#     dists_tid = Vector{typeof(dists1)}(undef, length(particle_tid))
+#     dists_tid[1] = dists1
+#     for tid in 2:length(particle_tid)
+#         dists_tid[tid] = compareDataVSimError(dataMetrics, particle_tid[tid].simResults)
+#     end
+#     return dists_tid
+# end
 
-function runABC(runModelSim::Function, compareDataVSim::Function, priorDists_pid::NamedTuple, dataMetrics, ctrlParams::Dict, nParticles)
+# function runABC(runModelSim::Function, compareDataVSim::Function, priorDists_pid::NamedTuple, dataMetrics, ctrlParams::Dict, nParticles)
 
-    particle_tid = runABCParticles(runModelSim, priorDists_pid, ctrlParams, nParticles)
+#     particle_tid = runABCParticles(runModelSim, priorDists_pid, nParticles, ctrlParams)
 
-    accepted_tid = testABCParticles(compareDataVSim, particle_tid, dataMetrics, ctrlParams[:thresholds])
+#     accepted_tid = testABCParticles(compareDataVSim, particle_tid, dataMetrics, ctrlParams[:thresholds])
 
-    return particle_tid, accepted_tid
-end
+#     return particle_tid, accepted_tid
+# end
 
-function acceptedParams(particle_tid, accepted_tid)
-    [particle.paramSet for particle in particle_tid[accepted_tid]]
-end
+# function acceptedParams(particle_tid, accepted_tid)
+#     [particle.paramSet for particle in particle_tid[accepted_tid]]
+# end
 
-function rankedParams(particle_tid, tid_rank, number::Union{Int,Nothing}=nothing)
-    if isnothing(number)
-        return [particle.paramSet for particle in particle_tid[tid_rank]]
-    else
-        return [particle.paramSet for particle in particle_tid[tid_rank[1:number]]]
-    end
-end
+# function rankedParams(particle_tid, tid_rank, number::Union{Int,Nothing}=nothing)
+#     if isnothing(number)
+#         return [particle.paramSet for particle in particle_tid[tid_rank]]
+#     else
+#         return [particle.paramSet for particle in particle_tid[tid_rank[1:number]]]
+#     end
+# end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,48 @@
 using ABCRejection
 using Test
+using Distributions
+using Random
 
 @testset "ABCRejection.jl" begin
-    # Write your tests here.
+    @testset "runParticle basic" begin
+
+        mockModel(p, ctrl) = p.a + p.b
+        inputParams = (a = 1.5, b = 2.5)
+        
+        particle = runParticle(mockModel, inputParams, (;))
+        
+        @test particle isa ABCRejection.Particle
+        @test particle.simResults == 4.0
+        @test particle.paramSet == inputParams
+    end
+
+    @testset "runABCParticles basics" begin
+        mockModel(p, ctrl) = p.a + p.b
+
+        priorDist_pid = (a=Normal(1.,1.), b=Normal(2.,2.))
+        parName_pid = keys(priorDist_pid) |> Tuple
+        nParticles = 100
+        pVal_tid_pid = stack(rand(priorDist, nParticles) for priorDist in values(priorDist_pid))
+        params_tid = [NamedTuple{parName_pid}(Tuple(@view pVal_tid_pid[tid, :])) for tid in 1:nParticles]
+
+        particle_tid = runABCParticles(mockModel, priorDist_pid, nParticles)
+        @test particle_tid isa Vector{ABCRejection.Particle}
+        @test length(particle_tid)==nParticles
+        @test particle_tid[1].paramSet isa @NamedTuple{a::Float64, b::Float64}
+        @test particle_tid[1].simResults isa Float64
+
+        particle_tid = runABCParticles(mockModel, params_tid)
+        @test particle_tid isa Vector{ABCRejection.Particle}
+        @test length(particle_tid)==nParticles
+        @test particle_tid[1].paramSet isa @NamedTuple{a::Float64, b::Float64}
+        @test particle_tid[1].simResults isa Float64
+
+        pVal_tid_Pid = (a=rand(priorDist_pid.a, nParticles), b=rand(priorDist_pid.b, nParticles))
+        particle_tid = runABCParticles(mockModel, pVal_tid_Pid)
+        @test particle_tid isa Vector{ABCRejection.Particle}
+        @test length(particle_tid)==nParticles
+        @test particle_tid[1].paramSet isa @NamedTuple{a::Float64, b::Float64}
+        @test particle_tid[1].simResults isa Float64
+    end
+
 end
